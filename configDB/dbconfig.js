9a9df7e504e37c84608dd3f16b56dbd948b361c2
N19DCCN013@student.ptithcm.edu.vn <N19DCCN013@student.ptithcm.edu.vn>
2023-08-24 19:08:15 +0700

    ...

--- configDB/dbconfig.js
+++ configDB/dbconfig.js
@@ -10,7 +10,7 @@ const config = {
     },
     options: {
         encrypt: false, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
+        trustServerCertificate: true // change to true for local dev / self-signed certs
     }
 }
 
