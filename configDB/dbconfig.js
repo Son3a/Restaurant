0d3345fa4d34a31b0c65e5db5c59147c8a86b70b
N19DCCN013@student.ptithcm.edu.vn <N19DCCN013@student.ptithcm.edu.vn>
2023-08-24 20:56:20 +0700

    fix db

--- configDB/dbconfig.js
+++ configDB/dbconfig.js
@@ -1,16 +1,33 @@
-const config = {
-    user: "sa",
-    password: "123",
-    server: "127.0.0.1",
-    database: "NHA_HANG",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
+// const config = {
+//     user: "sa",
+//     password: "123",
+//     server: "127.0.0.1",
+//     database: "NHA_HANG",
+//     port:"61427",
+//     pool: {
+//         max: 10,
+//         min: 0,
+//         idleTimeoutMillis: 30000
+//     },
+//     options: {
+//         encrypt: false, // for azure
+//         trustServerCertificate: false // change to true for local dev / self-signed certs
+//     }
+// }
+
+var config = {
+    server: 'localhost',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'sa', // update me
+            password: '123' // update me
+        }
     },
     options: {
-        encrypt: false, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
+        database: 'NHA_HANG',
+        validateBulkLoadParameters:false,
+        encrypt: false,
     }
 }
 
